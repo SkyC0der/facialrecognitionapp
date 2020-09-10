61fdba586fd311a8696ea27f7db9f89f29c46b68
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-09-11 00:11:30 +0100

    Updates

---
+++ precache-manifest.182b640c4d8c95f295190846fb1dcd11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b520aae0f8dec1495ae7cfb2db7106d",
+    "url": "/facialrecognitionapp/index.html"
+  },
+  {
+    "revision": "e223edd183680d1e9bdd",
+    "url": "/facialrecognitionapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b6fcbc5365a4db7742aa",
+    "url": "/facialrecognitionapp/static/css/main.4616f970.chunk.css"
+  },
+  {
+    "revision": "e223edd183680d1e9bdd",
+    "url": "/facialrecognitionapp/static/js/2.7e3f6876.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facialrecognitionapp/static/js/2.7e3f6876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6fcbc5365a4db7742aa",
+    "url": "/facialrecognitionapp/static/js/main.8e7373ec.chunk.js"
+  },
+  {
+    "revision": "ab4172442830a35b5036",
+    "url": "/facialrecognitionapp/static/js/runtime-main.4c164074.js"
+  },
+  {
+    "revision": "a571d66a4942a877f23e1ee41f46802a",
+    "url": "/facialrecognitionapp/static/media/brain.a571d66a.png"
+  }
+]);
